e68f444c5fd2ca306aa600cf9ffbb81385c12851
fennecdjay <astor.jeremie@wanadoo.fr>
2019-07-01 23:51:12 +0200

    :bug: Fix snippet in snippet

--- src/MdrSnip.hs
+++ src/MdrSnip.hs
@@ -53,12 +53,17 @@ findSnipk' ((a, b):xs) y
   | isInclude a y = Right b
   | otherwise = findSnipk' xs y
 
+skipFirstLine :: String -> String
+skipFirstLine (x:xs)
+  | x == '\n' = xs
+  | otherwise = skipFirstLine xs
+
 doExpand :: Snip -> Token -> MdrString
 doExpand k x =
   case b of
     Right nk ->
       case findSnipk' (knowResult nk) (tokStr x) of
-        Right a -> Right a
+        Right a -> Right $ fmap skipFirstLine a
         Left e  -> Left $ tokErr x e
     Left e -> Left e
   where
@@ -69,7 +74,7 @@ doExpand k x =
            (vis ++ [tokId x])
            ret
            (tailN (initN (tokStr x) 2) 3, return []))
-        (findSnip (knowTok k) x)
+        (findSnip tok x)
     tok = knowTok k
     vis = knowVisited k
     ret = knowResult k
